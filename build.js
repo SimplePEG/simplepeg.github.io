a7f3e88e704dae9c96e79df2f1ff1372ec5694e4
Oleksii Okhrymenko <ai_boy@live.ru>
2017-01-18 16:37:55 +0300

    source map + minification

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ var Builder = require('systemjs-builder');
 var builder = new Builder('./', 'config.js');
 
 builder
-    .buildStatic('src/main.ts', 'bundle.js')
+    .buildStatic('src/main.ts', 'bundle.js', { minify: true, sourceMaps: true })
     .then(function() {
         console.log('Build complete');
     })
