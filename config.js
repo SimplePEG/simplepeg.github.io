ad4de4eff4bd19c5e5e5ba9206b20ac8dc716014
Oleksii Okhrymenko <ai_boy@live.ru>
2017-01-18 21:26:53 +0300

    router

--- config.js
+++ config.js
@@ -24,6 +24,7 @@ System.config({
         '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
         '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
         '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
 
         'rxjs': 'npm:rxjs',
         'typescript': 'npm:typescript/lib/typescript.js'
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component'
+import { HomeComponent } from './components/home/home.component'
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
+
+const appRoutes: Routes = [
+    { 
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent
+    },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-	imports: [ BrowserModule, MaterialModule.forRoot() ],
-	declarations: [ AppComponent ],
+	imports: [ 
+        BrowserModule, 
+        MaterialModule.forRoot(), 
+        RouterModule.forRoot(appRoutes, { useHash: true })
+    ],
+	declarations: [
+        AppComponent,
+        HomeComponent,
+        PageNotFoundComponent
+    ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
