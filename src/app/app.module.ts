8583fceeb50d96e980360634c4ac6e5df4249af5
Oleksii Okhrymenko <ai_boy@live.ru>
2017-07-20 13:08:08 +0300

    use hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ const appRoutes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(
-      appRoutes // ,{ enableTracing: true }
+      appRoutes,  { useHash: true } // ,{ enableTracing: true }
     ),
     MdToolbarModule,
     MdButtonModule,
