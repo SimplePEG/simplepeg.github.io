4c109337cc61fb9d573bbdc5a502612c67c59976
irustm <jamakrus@gmail.com>
2021-11-09 00:40:00 +0300

    chore: reorder imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {HomeComponent} from "./home/home.component";
 import { LanguagesComponent } from './languages/languages.component';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   declarations: [
